d4b56a51abb5d8d3d993ef19a8ccf583ae653431
gsuter <gonzague1412@gmail.com>
2024-07-17 18:50:04 +0200

    cas env-i gere, a verifier sur les pc de lecole

--- exec/src/main.c
+++ exec/src/main.c
@@ -34,17 +34,18 @@ int	main(int arc, char **argv, char **env)
 	char		*input;
 	t_glob		glob;
 
-	ft_handle_signal();
 	(void)argv;
 	if (arc > 1)
 		return (printf("To much args\n"), g_error_code);
+	ft_handle_signal();
+	glob.env = NULL;
 	if (env && *env)
 		ft_get_env(&glob, env);
 	else
 		ft_main_get_env(&glob);
 	while (1)
 	{
-		input = readline("MinisHell-1.0$ "/*getcwd(NULL,0)*/);
+		input = readline("MinisHell-1.0$ ");
 		if (input == NULL)
 			return (ft_free_env(glob.env), g_error_code);
 		if (parse_in_struct(&glob, input) == 0)
